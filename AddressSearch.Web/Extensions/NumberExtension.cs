ce003499d11f1454fd2071d3749f7badbb4da921
Pedersen, Roy Viggo <roy.viggo.pedersen@cappelendamm.no>
2021-07-21 23:41:56 +0200

    Extension for period as decimal separator

---
+++ AddressSearch.Web/Extensions/NumberExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace AddressSearch.Web.Extensions
+{
+    public static class NumberExtension
+    {
+        public static string ToDecimalString(this double value)
+        {
+            return value.ToString(new NumberFormatInfo { NumberDecimalSeparator = "." });
+        }
+    }
+}
